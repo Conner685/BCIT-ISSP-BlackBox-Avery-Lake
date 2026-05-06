565b5c8407fbf88c549c4fa0c098ff8b10f98a13
Alex Hidalgo <cheesewheel64@gmail.com>
2026-05-06 12:03:28 -0700

    Initial touch sensor code.

--- BCIT-ISSP-BlackBox-Avery-Lake.ino
+++ BCIT-ISSP-BlackBox-Avery-Lake.ino
@@ -1,19 +1,37 @@
-// Different based on which board you're using
-#define PIN 14
+#define TOUCH_PIN T0
+#define R_PIN 23
+#define B_PIN 21
+#define G_PIN 22
+
+#define RED 0
+#define GREEN 212
+#define BLUE 255
+
+int threshold = 30; // might differ board to board
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("hello!");
-
-  pinMode(PIN, OUTPUT);
+  Serial.begin(115200);
+  ledcAttach(R_PIN, 5000, 8);
+  ledcAttach(G_PIN, 5000, 8);
+  ledcAttach(B_PIN, 5000, 8);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(PIN, HIGH);
-  Serial.println("LED on");
-  delay(1000);
-  digitalWrite(PIN, LOW);
-  Serial.println("LED off");
-  delay(1000);
+  int touchValue = touchRead(TOUCH_PIN);
+
+  Serial.println(touchValue);
+
+  if (touchValue < threshold) {
+    ledcWrite(R_PIN,
+              255 -
+                  RED); // may not need to subtract depending on type of rgb led
+    ledcWrite(G_PIN, 255 - GREEN);
+    ledcWrite(B_PIN, 255 - BLUE);
+  } else {
+    ledcWrite(R_PIN, 255); // off state, may be 0 depending on led
+    ledcWrite(G_PIN, 255);
+    ledcWrite(B_PIN, 255);
+  }
+
+  delay(50);
 }
