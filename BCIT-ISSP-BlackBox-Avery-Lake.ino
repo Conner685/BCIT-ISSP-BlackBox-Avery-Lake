1f51b0593d016e0e33a8ecd36fb81c5081a3a370
nillnought <gglaim@my.bcit.ca>
2026-05-13 11:04:52 -0700

    Update with fade logic

--- BCIT-ISSP-BlackBox-Avery-Lake.ino
+++ BCIT-ISSP-BlackBox-Avery-Lake.ino
@@ -1,14 +1,16 @@
 #define TOUCH_PIN T0
-#define R_PIN 23
-#define B_PIN 21
-#define G_PIN 22
+#define R_PIN 12
+#define G_PIN 14
+#define B_PIN 27
 
 #define RED 0
 #define GREEN 212
 #define BLUE 255
 
-int threshold = 30; // might differ board to board
+int threshold = 400; //might differ board to board
 
+float brightness = 0.0;
+float fadeSpeed = 0.05;
 void setup() {
   Serial.begin(115200);
   ledcAttach(R_PIN, 5000, 8);
@@ -22,16 +24,15 @@ void loop() {
   Serial.println(touchValue);
 
   if (touchValue < threshold) {
-    ledcWrite(R_PIN,
-              255 -
-                  RED); // may not need to subtract depending on type of rgb led
-    ledcWrite(G_PIN, 255 - GREEN);
-    ledcWrite(B_PIN, 255 - BLUE);
+    brightness += fadeSpeed;
+    if(brightness > 1.0) brightness = 1.0;
   } else {
-    ledcWrite(R_PIN, 255); // off state, may be 0 depending on led
-    ledcWrite(G_PIN, 255);
-    ledcWrite(B_PIN, 255);
+    brightness -= fadeSpeed;
+    if (brightness < 0.0) brightness = 0.0;
   }
+    ledcWrite(R_PIN, (int)(RED * brightness));
+    ledcWrite(G_PIN, (int)(GREEN * brightness));
+    ledcWrite(B_PIN, (int)(BLUE * brightness));
 
-  delay(50);
+  delay(10);
 }
