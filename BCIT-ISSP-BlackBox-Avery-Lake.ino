b2fcc7f4de209b95f147fe95b836a7f729c8412c
nillnought <gglaim@my.bcit.ca>
2026-05-13 12:22:50 -0700

    Update with threshold calculations

--- BCIT-ISSP-BlackBox-Avery-Lake.ino
+++ BCIT-ISSP-BlackBox-Avery-Lake.ino
@@ -7,23 +7,29 @@
 #define GREEN 212
 #define BLUE 255
 
-int threshold = 400; //might differ board to board
+int threshold = 700; //might differ board to board
 
 float brightness = 0.0;
 float fadeSpeed = 0.05;
 void setup() {
   Serial.begin(115200);
+  pinMode(T0, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(27, OUTPUT);
   ledcAttach(R_PIN, 5000, 8);
   ledcAttach(G_PIN, 5000, 8);
   ledcAttach(B_PIN, 5000, 8);
+
+  setBaseline();
 }
 
 void loop() {
   int touchValue = touchRead(TOUCH_PIN);
 
-  Serial.println(touchValue);
+  // Serial.println(touchValue);
 
-  if (touchValue < threshold) {
+  if (touchValue < (threshold - threshold/2.5) && touchValue > (threshold - threshold / 1.5)) {
     brightness += fadeSpeed;
     if(brightness > 1.0) brightness = 1.0;
   } else {
@@ -35,4 +41,13 @@ void loop() {
     ledcWrite(B_PIN, (int)(BLUE * brightness));
 
   delay(10);
+}
+
+void setBaseline(){
+  int total = 0;
+  for (int i = 0; i < 5; i++){
+    total += touchRead(TOUCH_PIN);
+  }
+  threshold = total/5;
+  Serial.println(threshold);
 }
